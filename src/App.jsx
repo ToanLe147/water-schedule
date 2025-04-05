7d0059a2279411c8554723f25c0fce206fe49e95
Toan Le <normalboyinlife@gmail.com>
2025-04-06 00:12:42 +0300

    Cung cung roi day

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
-import { Box } from '@mui/material'
+import { useState, useEffect } from 'react'
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
 
+import { Box, Paper, Fab } from '@mui/material'
 import { CssBaseline } from '@mui/material'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
+import LogoutIcon from '@mui/icons-material/Logout';
 
-import PlantCard from './components/PlantCard'
+import MainContent from './components/MainContent'
 
+
+const supabase = createClient(import.meta.env.VITE_APP_SUPABASE_URL, import.meta.env.VITE_APP_SUPABASE_ANON_KEY)
 
 function App() {
   const theme = createTheme({
@@ -23,23 +29,139 @@ function App() {
     },
   })
 
-  return (
-    <ThemeProvider theme={theme}>
+  const [session, setSession] = useState(null)
+  const [session_event, setSessionEvent] = useState(null)
 
-      <CssBaseline />
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
 
-      <Box sx={{
-        height: "100vh",
-        width: "100vw",
-        display: 'flex',
-      }}>
-        <PlantCard />
-        <PlantCard />
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      if (_event === 'SIGNED_IN') {
+        setSession(session)
+        setSessionEvent(null)
+      }
+      if (_event === 'SIGNED_OUT') {
+        setSession(null)
+        setSessionEvent(null)
+      }
+      if (_event === 'PASSWORD_RECOVERY') {
+        setSession(null)
+        setSessionEvent(_event)
+      }
+    })
 
-      </Box>
+    return () => subscription.unsubscribe()
+  }, [])
 
-    </ThemeProvider>
-  )
+  console.log('session', session)
+
+  if (!session) {
+    return (
+
+      <ThemeProvider theme={theme}>
+
+        <CssBaseline />
+
+        <Box sx={{
+          height: "100vh",
+          width: "100vw",
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          color: 'text.primary',
+          backgroundColor: '#FFCDD2',
+        }}>
+          <Paper elevation={3} sx={{
+            padding: 10,
+            minHeight: '50%',
+            minWidth: '50%',
+            objectFit: 'contain',
+          }}>
+            <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme='default' providers={[]} />
+          </Paper>
+        </Box>
+
+      </ThemeProvider>
+    )
+  }
+
+  else if (session_event === 'PASSWORD_RECOVERY') {
+    return (
+
+      <ThemeProvider theme={theme}>
+
+        <CssBaseline />
+
+        <Box sx={{
+          height: "100vh",
+          width: "100vw",
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          color: 'text.primary',
+          backgroundColor: '#FFCDD2',
+        }}>
+          <Paper elevation={3} sx={{
+            padding: 10,
+            minHeight: '50%',
+            minWidth: '50%',
+            objectFit: 'contain',
+          }}>
+            <Auth
+              supabaseClient={supabase}
+              appearance={{ theme: ThemeSupa }}
+              theme='default'
+              providers={[]}
+              view='update_password'
+            />
+          </Paper>
+        </Box>
+
+      </ThemeProvider>
+    )
+  }
+
+  else {
+
+    return (
+      <ThemeProvider theme={theme}>
+
+        <CssBaseline />
+
+        <Box sx={{
+          height: "100vh",
+          width: "100vw",
+          display: 'flex',
+        }}>
+          <Box sx={{
+            padding: 2,
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            color: 'text.primary',
+            backgroundColor: '#FFCDD2',
+          }}>
+            <MainContent />
+          </Box>
+          <Fab color="secondary" aria-label="out" onClick={() => supabase.auth.signOut()}
+            sx={{
+              position: 'absolute',
+              bottom: 20,
+              right: 20,
+            }}>
+            <LogoutIcon />
+          </Fab>
+        </Box>
+
+      </ThemeProvider>
+    )
+  }
 }
 
 export default App
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+import PlantCard from './PlantCard';
+
+
+export default function MainContent() {
+  return (
+    <Box sx={{
+       flexGrow: 1,
+       }}>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ sm: 1, md: 8, lg: 12 }}>
+        {Array.from(Array(6)).map((_, index) => (
+          <Grid key={index} size={{ xs: 2, sm: 4, md: 4 }}>
+            <PlantCard />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+}
