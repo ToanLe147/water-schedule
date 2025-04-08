f9e3f1bce62c86e67beb80aec3784d3c2d3aa76b
Toan Le <normalboyinlife@gmail.com>
2025-04-09 00:14:27 +0300

    Tuning Plant Page

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,29 @@
-// import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
+import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 import Fade from '@mui/material/Fade';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import Divider from '@mui/material/Divider';
+
+
+export const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
 
 
 export default function PlantPage({
@@ -27,6 +42,8 @@ export default function PlantPage({
     setOpen(false);
   };
 
+  const [editMode, setEditMode] = useState(true);
+
   return (
     <>
       <Fade in={open}>
@@ -35,11 +52,11 @@ export default function PlantPage({
           keepMounted
           onClose={handleClose}
           aria-describedby="alert-dialog-slide-description"
-          // sx={{
-          //   border: '2px solid #81C784',
-          // }}
+        // sx={{
+        //   border: '2px solid #81C784',
+        // }}
         >
-          <DialogTitle variant='h4' alignSelf='center' >{"Details View"}</DialogTitle>
+          <DialogTitle variant='h4' alignSelf='center' >{name}</DialogTitle>
           <Box
             component="img"
             sx={{
@@ -52,36 +69,71 @@ export default function PlantPage({
             loading='lazy'
             src={plantImageURL}
           />
+          <Button
+            component="label"
+            variant="contained"
+            size='small'
+            tabIndex={-1}
+            startIcon={<CloudUploadIcon />}
+            sx={{
+              width: 'fit-content',
+              alignSelf: 'center',
+              marginTop: 2,
+            }}
+          >
+            Upload image
+            <VisuallyHiddenInput
+              type="file"
+              onChange={(event) => console.log(event.target.files)}
+              multiple
+            />
+          </Button>
+          <Divider sx={{ width: '100%', marginTop: 2 }}>
+            Plant Info
+          </Divider>
           <DialogContent
             sx={{
               display: 'flex',
               flexDirection: 'column',
-              alignItems: 'begin',
-              justifyContent: 'space-between',
+              gap: 2,
+              overflow: 'auto',
             }}
           >
-            <Typography gutterBottom variant="h5" component="div">
-              {name}
-            </Typography>
-            <DialogContentText id="alert-dialog-slide-description">
-              {"ID: " + plantID}
-            </DialogContentText>
-            <Typography variant="body3" sx={{ color: 'text.secondary' }}>
-              Scientific Name: {scientificName}
-            </Typography>
-            <Typography variant="body3" sx={{ color: 'text.secondary' }}>
-              Drinking Days: {drinkingDay} days
-            </Typography>
-            <Typography variant="body3" sx={{ color: 'text.secondary' }}>
-              Last Watered: {wateringDate}
-            </Typography>
-            <Typography variant="body3" sx={{ color: 'text.secondary' }}>
-              Drinking Portion: {drinkingPortion} ml
-            </Typography>
+            <TextField
+              disabled={editMode}
+              label="ID"
+              variant="standard"
+              defaultValue={plantID}
+            />
+            <TextField
+              disabled={editMode}
+              label="Scientific Name"
+              variant="standard"
+              defaultValue={scientificName}
+            />
+            <TextField
+              disabled={editMode}
+              label="Drinking Day"
+              variant="standard"
+              defaultValue={drinkingDay}
+            />
+            <TextField
+              disabled={editMode}
+              label="Drinking Portion"
+              variant="standard"
+              defaultValue={drinkingPortion}
+            />
+            <TextField
+              disabled={editMode}
+              label="Last Watered"
+              variant="standard"
+              defaultValue={wateringDate}
+            />
           </DialogContent>
           <DialogActions>
-            <Button onClick={handleClose}>Close</Button>
-            <Button onClick={handleClose}>Also Close</Button>
+            <Button onClick={() => setEditMode((prevMode) => !prevMode)}>Edit</Button>
+            <Button hidden={!editMode} onClick={handleClose}>Close</Button>
+            <Button hidden={editMode} onClick={handleClose}>Submit</Button>
           </DialogActions>
         </Dialog>
       </Fade >
